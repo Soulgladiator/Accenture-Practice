ad5ad13707b1a64774093a7b0686af26bef1f2e5
SUBASH BOOPATHI <98462377+Soulgladiator@users.noreply.github.com>
2024-10-09 09:29:37 +0530

    Add files via upload

---
+++ day10a.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class solution{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		double result = Math.PI*d*d;
+		int result1 = (int) result;
+		System.out.println(result1);
+	}
+}
